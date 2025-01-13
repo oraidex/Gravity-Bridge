53017e8331a930eb4460768d6634104a782c3290
dang.vh <dang.vh2@orai.io>
2025-01-13 11:02:45 +0700

    add readme for upgrade handler

---
+++ module/app/upgrades/monitorevent/README.md
@@ -0,0 +1,8 @@
+# monitorevent UPGRADE
+
+The _Monitor Event_ upgrade contains the following changes.
+
+## Summary of Changes
+
+- Add event for tracking packet receive from EVM chain
+
--- module/scripts/e2e/upgrade.sh
+++ module/scripts/e2e/upgrade.sh
@@ -3,9 +3,9 @@ set -ux
 
 # setup the network using the old binary
 
-OLD_VERSION=${OLD_VERSION:-"v1.0.3"}
+OLD_VERSION=${OLD_VERSION:-"v1.0.7"}
 ARGS="--chain-id testing -y --keyring-backend test --fees 200uoraib --gas auto --gas-adjustment 1.5 -b block"
-NEW_VERSION=${NEW_VERSION:-"txidevent"}
+NEW_VERSION=${NEW_VERSION:-"monitorevent"}
 VALIDATOR_HOME=${VALIDATOR_HOME:-"$HOME/.gravity/validator1"}
 
 # kill all running binaries
@@ -21,29 +21,29 @@ cd $current_dir
 sh $PWD/scripts/multi_local_node.sh
 
 # test send from cosmos to eth
-gravity tx gravity send-to-eth 0x0000000000000000000000000000000000071001 12500000000000000000oraib0x0000000000000000000000000000000000C0FFEE 1000000000000000000oraib0x0000000000000000000000000000000000C0FFEE 2500000000000000oraib0x0000000000000000000000000000000000C0FFEE oraib --fees 10uoraib  --from orchestrator1 --chain-id testing --home $HOME/.gravity/validator1 --keyring-backend test -y -b block
+#gravity tx gravity send-to-eth 0x0000000000000000000000000000000000071001 12500000000000000000oraib0x0000000000000000000000000000000000C0FFEE 1000000000000000000oraib0x0000000000000000000000000000000000C0FFEE 2500000000000000oraib0x0000000000000000000000000000000000C0FFEE oraib --fees 10uoraib  --from orchestrator1 --chain-id testing --home $HOME/.gravity/validator1 --keyring-backend test -y -b block
 
 # test send request batch, can not set this on because client cli of this request-batch
 # at v1.0.3 version is wrong format
 # gravity tx gravity request-batch oraib0x0000000000000000000000000000000000C0FFEE oraib --fees 10uoraib  --from orchestrator1 --chain-id testing --home $HOME/.gravity/validator1 --keyring-backend test -y
 
 # create new upgrade proposal, turn this on when we need real proposal
-# UPGRADE_HEIGHT=${UPGRADE_HEIGHT:-19}
-# gravity tx gov submit-proposal software-upgrade $NEW_VERSION --title "foobar" --description "foobar"  --from validator1 --upgrade-height $UPGRADE_HEIGHT --upgrade-info "x" --deposit 10000000uoraib $ARGS --home $VALIDATOR_HOME
-# gravity tx gov vote 1 yes --from validator1 --home "$HOME/.gravity/validator1" $ARGS && gravity tx gov vote 1 yes --from validator2 --home "$HOME/.gravity/validator2" $ARGS
+UPGRADE_HEIGHT=${UPGRADE_HEIGHT:-19}
+gravity tx gov submit-proposal software-upgrade $NEW_VERSION --title "foobar" --description "foobar"  --from validator1 --upgrade-height $UPGRADE_HEIGHT --upgrade-info "x" --deposit 10000000uoraib $ARGS --home $VALIDATOR_HOME
+gravity tx gov vote 1 yes --from validator1 --home "$HOME/.gravity/validator1" $ARGS && gravity tx gov vote 1 yes --from validator2 --home "$HOME/.gravity/validator2" $ARGS
 
 # # sleep to wait til the proposal passes
-# echo "Sleep til the proposal passes..."
-# sleep 12
+echo "Sleep til the proposal passes..."
+sleep 12
 
 # # Check if latest height is less than the upgrade height
-# latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')
-# while [ $latest_height -lt $UPGRADE_HEIGHT ];
-# do
-#    sleep 5
-#    ((latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')))
-#    echo $latest_height
-# done
+latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')
+while [ $latest_height -lt $UPGRADE_HEIGHT ];
+do
+   sleep 5
+   ((latest_height=$(curl --no-progress-meter http://localhost:1317/blocks/latest | jq '.block.header.height | tonumber')))
+   echo $latest_height
+done
 
 # kill all processes
 pkill gravity
